d4a59ba75c932f65b0891da633a32147e4cdfd5f
FlorrieK <potteremail1@gmail.com>
2020-02-07 10:32:11 -0500

    Add app.py

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index.html')
+
+@app.route('/index')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def home():
+    return render_template('about.html')
 
 
 
